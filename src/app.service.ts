697957a0b60260fed73d493171aae4c9e792c8f7
Pan Pan <selypan@foxmail.com>
2021-07-01 16:54:03 +0800

    升级 ssh2依赖

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/websockets';
 import { promisify } from 'util';
 import * as Path from 'path';
-import { Config as ConnectionConfig, NodeSSH } from 'node-ssh';
+import { Config as ConnectionConfig, NodeSSH } from './utils/nodeSSH';
 import { ClientChannel } from 'ssh2';
 import * as _ from 'lodash';
 import * as moment from 'moment';
@@ -679,7 +679,70 @@ export class AppService
           // nothing
         }
       }
-      const connection = await new NodeSSH().connect(config);
+      const connection = await new NodeSSH().connect({
+        ...config,
+        algorithms: {
+          kex: [
+            'curve25519-sha256',
+            'curve25519-sha256@libssh.org',
+            'ecdh-sha2-nistp256',
+            'ecdh-sha2-nistp384',
+            'ecdh-sha2-nistp521',
+            'diffie-hellman-group-exchange-sha256',
+            'diffie-hellman-group14-sha256',
+            'diffie-hellman-group15-sha512',
+            'diffie-hellman-group16-sha512',
+            'diffie-hellman-group17-sha512',
+            'diffie-hellman-group18-sha512',
+            'diffie-hellman-group-exchange-sha1',
+            'diffie-hellman-group14-sha1',
+            'diffie-hellman-group1-sha1',
+          ],
+          serverHostKey: [
+            'ssh-ed25519',
+            'ecdsa-sha2-nistp256',
+            'ecdsa-sha2-nistp384',
+            'ecdsa-sha2-nistp521',
+            'rsa-sha2-512',
+            'rsa-sha2-256',
+            'ssh-rsa',
+            'ssh-dss',
+          ],
+          cipher: [
+            'chacha20-poly1305@openssh.com',
+            'aes128-gcm',
+            'aes128-gcm@openssh.com',
+            'aes256-gcm',
+            'aes256-gcm@openssh.com',
+            'aes128-ctr',
+            'aes192-ctr',
+            'aes256-ctr',
+            '3des-cbc',
+            'aes256-cbc',
+            'aes192-cbc',
+            'aes128-cbc',
+            'arcfour256',
+            'arcfour128',
+            'arcfour',
+            'blowfish-cbc',
+            'cast128-cbc',
+          ],
+          hmac: [
+            'hmac-sha2-256-etm@openssh.com',
+            'hmac-sha2-512-etm@openssh.com',
+            'hmac-sha1-etm@openssh.com',
+            'hmac-sha2-256',
+            'hmac-sha2-512',
+            'hmac-sha1',
+            'hmac-md5',
+            'hmac-sha2-256-96',
+            'hmac-sha2-512-96',
+            'hmac-ripemd160',
+            'hmac-sha1-96',
+            'hmac-md5-96',
+          ],
+        },
+      });
 
       connection.connection?.on('error', (error) => {
         this.logger.error('connection server error', error.stack);
